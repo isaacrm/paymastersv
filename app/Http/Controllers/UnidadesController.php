c08fde1955c3166a0cb193f0cf72eec9d96f4a30
Rosalioalfre2 <rosalioalfre2@gmail.com>
2023-06-19 18:03:36 -0600

    Bug unidades resuelto

--- app/Http/Controllers/UnidadesController.php
+++ app/Http/Controllers/UnidadesController.php
@@ -31,7 +31,7 @@ class UnidadesController extends Controller
             ->leftJoin('centro_de_costos as c', 'u1.centro_costos_id', '=', 'c.id')
             ->select('u1.id', 'u1.nombre', 'u1.superior_id', 'u1.centro_costos_id', 'u1.nivel_organizacional', 'u2.nombre as nivel_organizacional_nombre', 'p.nombre as superior_nombre', 'c.anyo as centro_costos_año', 'c.nombre as centro_costos_nombre')
             ->where(function ($query) use ($filtro){
-                $query->where('u1.nombre', '%'. $filtro. '%')
+                $query->where('u1.nombre', 'like', '%'. $filtro. '%')
                 ->orWhere('u2.nombre', 'like', '%' . $filtro . '%')
                 ->orWhere('c.anyo', 'like', '%' . $filtro . '%')
                 ->orWhere('c.nombre', 'like', '%' . $filtro . '%');
